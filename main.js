f6b15847b9b2aa58e2331992ada4b32ed27f2399
Lavya5586 <78597821+Lavya5586@users.noreply.github.com>
2021-11-16 17:53:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+status = "";
+
+function setup()
+{
+    canvas = createCanvas(480, 350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    bustart = document.getElementById("input").nodeValue;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
